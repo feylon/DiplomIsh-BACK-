1f16b2d4028d534e30b6dce7f415df76f5a4356e
feylon <jamshid14092002@gmail.com>
2025-05-13 15:10:11 +0500

    fix: correct import path for gethisory module

--- Routers/Student/index.js
+++ Routers/Student/index.js
@@ -1,6 +1,6 @@
 import genarateQrcode from "./QRcode/genarateQrcode.js";
 import GetBio from "./GetBio.js";
-import gethisory from "./History/genarateQrcode.js";
+import gethisory from "./HIstory/genarateQrcode.js";
 
 const routers = [
     {path : "/getqr", router : genarateQrcode},
